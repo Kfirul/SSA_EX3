8c18edc3fe7b38e5c05954bd4094d91488185686
Kfir Zilbernagel <98819583+Kfirul@users.noreply.github.com>
2022-12-22 17:23:33 +0200

    Add files via upload

--- isort.c
+++ isort.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #define SIZE 50
 
-void shift_element(int* arr, int i){
+void swap(int *a,int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
+void shift_element(int* arr, int i){
+    for (int j = i; j>0; j--) {
+        swap(arr+j, arr+j+1);
+    }
 }
 
 void insertion_sort(int* arr , int len){
 
+    for(int i=0;i< len-1;i++){
+        int count =0;
+        if(*(arr+i)> *(arr+i+1)){
+            int j=i;
+            int temp=*(arr+i+1);
+        while(j>-1 && *(arr+j) > *(arr+i+1)){
+            count++;
+            j--;
+        }
+        shift_element(arr+j,count);
+        *(arr+j)=temp;
+        }
+    }
 }
 
 int main(){
@@ -16,12 +37,11 @@ while(i<SIZE){
     scanf("%d",(arr+i));
     i++;
 }
-// insertion_sort(0,50);
+insertion_sort(arr,50);
 
 for(int j=0;j<SIZE-1;j++){
     printf("%d,",*(arr+j));
 }
 printf("%d",*(arr+SIZE-1));
-return 1;
-
+return 0;
 }
