d7e589f57275686409a5eb7c20604caa4ab3539c
royofer123 <royofer123@gmail.com>
2022-12-29 17:34:24 +0200

    Restore

--- isort.c
+++ isort.c
@@ -1,42 +1,48 @@
 #include <stdio.h>
-#define SIZE 50;
+#define SIZE 50
 
+void swap(int *a,int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 void shift_element(int* arr, int i){
-    for ( int k = i-1 ; k >=0 ; k--){
-        int temp = *(arr+k);
-        *(arr+k) = *(arr+k+1);
-        *(arr+k+1) = temp;
+    for (int j = i; j>0; j--) {
+        swap(arr+j, arr+j+1);
     }
 }
 
-void insertion_sort(int* arr, int len){
-    for (int i = 1; i < len ; i++){
-       int current = *(arr+i);
-       int j = i-1;
+void insertion_sort(int* arr , int len){
 
-       while (j>=0 && *(arr+ j) > current){
-            shift_element(arr+j, 1);
+    for(int i=0;i< len-1;i++){
+        int count =0;
+        if(*(arr+i)> *(arr+i+1)){
+            int j=i;
+            int temp=*(arr+i+1);
+        while(j>-1 && *(arr+j) > *(arr+i+1)){
+            count++;
             j--;
-       }
-       *(arr+j+1) = current;
+        }
+        shift_element(arr+j,count);
+        *(arr+j)=temp;
+        }
     }
 }
+
 int main(){
-    int arr[SIZE]; 
-    for (int i = 0; i < SIZE ;i++)
-    {
-       scanf("%d",&arr[i]);
-    }
-    insertion_sort(arr, SIZE);
-    for (int i = 0 ; i < SIZE ; i++){
-        if (i < SIZE-1){
-            printf("%d,", *(arr+i));
-        }
-        else if (i==SIZE-1){
-            printf("%d", *(arr+i));
-        }
-    }
-	printf("\n");
-  return 0;
+int arr[SIZE];
+int i=0;
+while(i<SIZE){
+    scanf("%d",(arr+i));
+    i++;
+}
+insertion_sort(arr,50);
+
+for(int j=0;j<SIZE-1;j++){
+    printf("%d,",*(arr+j));
+}
+printf("%d",*(arr+SIZE-1));
+printf("/n");
+return 0;
 }
