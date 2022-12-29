6f50f9b286fca8552015f50e1e8ff2b5196d61a8
royofer123 <royofer123@gmail.com>
2022-12-29 17:18:18 +0200

    fix isort

--- isort.c
+++ isort.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
-#define SIZE 50
+#define SIZE 50;
 
-void swap(int *a,int *b){
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void shift_element(int* arr, int i)
+{
+	int temp = *(arr + 1), temp1;
+
+	for (int j = 1; j <= i; j++)
+	{
+		temp1 = *(arr + j + 1);
+		*(arr + j + 1) = temp;
+		temp = temp1;
+
+	}
+	return;
+}
+void insertion_sort(int* arr, int len)
+{
+	int i, temp, j;
+	int counter = 0;
+	for (i = 1; i < len; i++) {
+		counter = 0;
+		temp = *(arr + i);
+		j = i - 1;
+
+		while (j >= 0 && *(arr + j) > temp) {
+			counter++;
+			j = j - 1;
+		}
+		shift_element(arr + j, counter);
+		*(arr + j + 1) = temp;
+	}
 }
 
-void shift_element(int* arr, int i){
-    for (int j = i; j>0; j--) {
-        swap(arr+j, arr+j+1);
-    }
-}
-
-void insertion_sort(int* arr , int len){
-
-    for(int i=0;i< len-1;i++){
-        int count =0;
-        if(*(arr+i)> *(arr+i+1)){
-            int j=i;
-            int temp=*(arr+i+1);
-        while(j>-1 && *(arr+j) > *(arr+i+1)){
-            count++;
-            j--;
-        }
-        shift_element(arr+j,count);
-        *(arr+j)=temp;
-        }
-    }
-}
 
 int main(){
  int arr[SIZE];
