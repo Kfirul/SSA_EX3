561c80bc8128fac6ead88ce2d80d057b969060e9
royofer123 <royofer123@gmail.com>
2022-12-29 17:22:37 +0200

    isort2

--- isort.c
+++ isort.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
 #define SIZE 50;
 
-void shift_element(int* arr, int i)
-{
-	int temp = *(arr + 1), temp1;
 
-	for (int j = 1; j <= i; j++)
-	{
-		temp1 = *(arr + j + 1);
-		*(arr + j + 1) = temp;
-		temp = temp1;
-
-	}
-	return;
-}
-void insertion_sort(int* arr, int len)
-{
-	int i, temp, j;
-	int counter = 0;
-	for (i = 1; i < len; i++) {
-		counter = 0;
-		temp = *(arr + i);
-		j = i - 1;
-
-		while (j >= 0 && *(arr + j) > temp) {
-			counter++;
-			j = j - 1;
-		}
-		shift_element(arr + j, counter);
-		*(arr + j + 1) = temp;
-	}
+void shift_element(int* arr, int i){
+    for ( int k = i-1 ; k >=0 ; k--){
+        int temp = *(arr+k);
+        *(arr+k) = *(arr+k+1);
+        *(arr+k+1) = temp;
+    }
 }
 
+void insertion_sort(int* arr, int len){
+    for (int i = 1; i < len ; i++){
+       int current = *(arr+i);
+       int j = i-1;
+
+       while (j>=0 && *(arr+ j) > current){
+            shift_element(arr+j, 1);
+            j--;
+       }
+       *(arr+j+1) = current;
+    }
+}
 int main(){
     int arr[SIZE]; 
     for (int i = 0; i < SIZE ;i++)
