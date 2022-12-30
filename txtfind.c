c6215d0b463d3f68e95ba06ce0fbd955ffabbc43
Kfir Zilbernagel <98819583+Kfirul@users.noreply.github.com>
2022-12-30 14:47:56 +0200

    Add files via upload

--- txtfind.c
+++ txtfind.c
@@ -5,15 +5,13 @@
 #define NUMLINES 250
 
 int getLine(char s[]){
-  char c= 0 ;
-  for(int i=0; i<LINE; i++){
-    if(scanf("%c",&c)==EOF || c=='\n' || c == '\r' ){
-        s[i]== '\0';
-        break;     
-    }
-    else s[i]=c;
-  }
-  return strlen(s);
+  int i=0;
+  while(scanf("%c",&s[i])!=EOF && s[i]!= '\n' &&s[i]!= '\r'&& i < WORD){
+    i++;
+  } 
+  
+s[i]='\0';
+return i;
 }
 
 
@@ -78,8 +76,6 @@ void print_similar_words(char * str){
             printf("%s\n", current);
     }
 }
-
-
 
 
 void printLine(char* str){
