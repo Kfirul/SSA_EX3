c5b3b694913b863418dec5a7c3b8e6fde79845ce
royofer123 <royofer123@gmail.com>
2022-12-30 12:36:44 +0200

    fix

--- txtfind.c
+++ txtfind.c
@@ -18,20 +18,19 @@ int getLine(char s[]){
 
 
 int getWord(char w[]){
-  {
-    int count = 0;
-    for(int i =0; i < WORD; i++)
-    {
-        scanf("%c",&w[i]);
-        count++;
-        if(w[i] == '\n' || w[i] == '\t' || w[i] == ' ' || w[i]=='\r')
-        {
-            w[i]='\0';
-            break;
+  int count = 0;
+    while (count<WORD){
+        scanf("%c", &(w[count]));
+        if (w[count]!= '\n' && w[count]!= '\t' && w[count]!= ' '&& w[count]!= '\r'){
+                    count++;
         }
+        else{
+            w[count] = '\0';
+            count++;
+            break;
+        }       
     }
-    return count; 
-}
+    return count;
 int substring(char* str1, char* str2){
     int j;
     for (int i = 0 ; i < strlen(str1)-strlen(str2)+1 ; i++){
