019e9a689f649274d9def0b6342db7b0a21f69cf
royofer123 <royofer123@gmail.com>
2022-12-30 11:22:24 +0200

    txtfind fixes

--- functions.h
+++
@@ -1,9 +0,0 @@
-void shift_element(int* arr, int i);
-void insertion_sort(int* arr , int len);
-int getLine(char s[]);
-int getword(char w[]);
-int similar (char *s, char *t);
-int getword(char w[]);
-int substring(char* str1, char* str2);
-void print_lines(char * str);
-void print_similar_words(char * str);
